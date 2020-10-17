1d4b4741142235db86d0832c33f7095e17ae0e5e
Avinash2077 <72973307+Avinash2077@users.noreply.github.com>
2020-10-17 20:58:30 +0530

    Assaignment 1 of employee details

---
+++ employee details/Employee.java
@@ -0,0 +1,26 @@
+public class employee{
+		
+		String name=new String("avinash");
+		String city=new String("ongole");
+		int age=20;
+		
+		public void display(){
+
+				System.out.println("name of the student  :"+e1.name);
+				
+				
+			System.out.println("students city  :"+e1.city);
+			
+			System.out.println("age of the student  :"e1.+20);
+			
+			}
+			
+			public static  void main(String args[]){
+			
+			employee e1=new employee();
+			e1.display():
+			}
+			
+			
+	}		
+		
---
+++ employee details/Main.java
@@ -0,0 +1,17 @@
+package com.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code her
+
+
+        Employee e1=new Employee();
+
+        e1.name="avinash";
+        e1.city="ongole";
+        e1.age=58;
+
+        e1.display();
+        }
+}
